525855f30abf66be9777121c15299d662d36cbd9
Ji Woong Park <jiwoong-macbook-air@Jis-MacBook-Air.lan>
2025-05-12 16:46:36 -0400

    Fixing recent logs

--- components/RecentLogs.tsx
+++ components/RecentLogs.tsx
@@ -3,12 +3,18 @@ import { cookies } from "next/headers"
 
 export default async function RecentLogs() {
 
+    const cookieStore = cookies()
+
     const supabase = createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        { 
-            cookies: () => cookies(),
-         }
+        {
+            cookies: {
+                get(name) {
+                    return cookieStore.get(name)?.value
+                },
+            },
+        }
     )
 
     const { data: logs, error } = await supabase
