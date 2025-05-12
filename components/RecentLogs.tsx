58b42189970de7fe49de7b29bebea25f89a78d1a
Ji Woong Park <jiwoong-macbook-air@Jis-MacBook-Air.lan>
2025-05-12 17:00:17 -0400

    Cleaned up unused vars

--- components/RecentLogs.tsx
+++ components/RecentLogs.tsx
@@ -1,5 +1,3 @@
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-import { cookies } from "next/headers"
 import { createClient } from "@supabase/supabase-js"
 
 export default async function RecentLogs() {
